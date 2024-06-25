3a3effeef797899af3a2813690bac4928dfd1707
SirRango <115499424+WhosDouble@users.noreply.github.com>
2024-06-25 12:26:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const navsection = document.querySelector(".nav-section");
+const navlinks = document.querySelector(".nav-links");
+
+navlinks.style.paddingRight =
+  navlinks.offsetWidth - navlinks.clientWidth + "px";
